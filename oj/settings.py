116ed0542c5b50fa017a9f7083b5d14ffdd21d02
harshkh-001 <harshkhandelwal1245@gmail.com>
2025-07-27 17:40:25 +0530

    updating

--- oj/settings.py
+++ oj/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,12 +86,20 @@ WSGI_APPLICATION = 'oj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+# import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default="postgresql://oj_db_gkm6_user:YpaQ2EBleZhZUaspjVIuaCEUbIfUq3LG@dpg-d230vb2dbo4c73fne5d0-a.oregon-postgres.render.com/oj_db_gkm6"
+    )
 }
+
 
 
 # Password validation
